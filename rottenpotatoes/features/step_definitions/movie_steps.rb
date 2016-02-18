0af5f848db05350e2e448751e292fb4638f4bed8
Junhyung Kim <kimjunh4u@gmail.com>
2016-02-18 07:33:23 +0000

    part 1 done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    to_add_movie = Movie.new('title':movie['title'], 'rating': movie['rating'], 'release_date': movie['release_date'])
+    to_add_movie.save!
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,6 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
